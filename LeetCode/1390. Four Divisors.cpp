9bc0dda0be1b77dada2cddf34dc79e504d7640f5
DEmmaRL <demmarlmiau@gmail.com>
2024-02-05 02:31:31 -0600

    leetcode dir

---
+++ LeetCode/1390. Four Divisors.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define f first
+#define s second
+
+//typedef
+typedef long long int lli ;
+
+//fore
+#define fore(i, a, b) for(int i = (a); i <= (b); ++i)
+
+
+//ESTRUCTURAS
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define mp make_pair
+
+//ABREVIATURAS
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+
+#define CODED_BY_DEMMARL ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+//FUNCIONES
+lli gcd(lli a,lli b) { if (b==0) return a; return gcd(b, a%b); }
+lli lcm(lli a,lli b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(lli a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+
+
+// g++ main.cpp -o a && ./a < in > out
+
+
+int sumFourDivisors(vector<int>& nums) {
+        long long res = 0;
+
+        for(int i=0; i<nums.size(); ++i)
+        {
+            vector<int>v;
+
+            for(int j=1; j*j <= nums[i]; ++j)
+            {
+                if( nums[i] % j == 0 )
+                {
+                    v.push_back(j);
+
+                    if( nums[i]/j != j)
+                    {
+                        v.push_back(nums[i]/j);
+                    }
+                }
+                if(v.size()>4)
+                {
+                    break;
+                }
+            }
+            if(v.size()==4)
+            {
+                for(int j=0; j<v.size(); ++j)
+                {
+                    res+=v[j];
+                }
+            }
+        }
+
+        cout<<res<<endl;
+
+
+    }
+
+void solve( vector<int> v ) {
+    sumFourDivisors(v);
+}
+
+
+int main()
+{
+    CODED_BY_DEMMARL;
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+
+    int n;
+    cin>>n;
+
+    vector<int>v(n);
+
+    fore( i, 0 , n )
+    {
+        cin>>v[i];
+    }
+
+    solve(v);
+
+}
